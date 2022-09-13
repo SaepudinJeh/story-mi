be3005984095b1fc9a864147d51116072ccc5a03
SaepudinJeh <saepudinjeh@gmail.com>
2022-09-13 19:40:23 +0700

    update panel

--- src/components/homepages/PanelPiece.vue
+++ src/components/homepages/PanelPiece.vue
@@ -1,6 +1,7 @@
 <script setup>
   import moment from 'moment/min/moment-with-locales'
-import { effect } from '../../utils/randomAos';
+  import TemplatePanel from './TemplatePanel.vue';
+
   moment.locale('id')
 
   defineProps(['data'])
@@ -9,51 +10,39 @@ import { effect } from '../../utils/randomAos';
     return moment(data).format('dddd, LL')
   }
 
-  const getDataUser = JSON.parse(localStorage.getItem('user'))?.username || ''
-
 </script>
 
 <template>
-  <section class="w-full h-screen flex items-center justify-center">
-    <div
-      class="flex items-center flex-col gap-y-5"
-      data-aos-duration="500"
-      data-aos-offset="100"
-      :data-aos="effect"
-    >
-      <img
-        v-if="data?.image[0]?.url"
-        v-lazy="data && data?.image[0]?.url"
-        class="h-36 w-36 xs:w-40 xs:h-40 sm:w-48 sm:h-48 md:w-52 md:h-52  rounded-full object-cover shadow-lg"
-        :alt="data?.title"
-        data-aos-anchor-placement="top-center"
-      >
-
-      <div class="mx-10 lg:mx-[400px] md:mx-[100px] text-center break-all">
-        <h2
-          class="md:text-xl text-base xs:text-base"
-          data-aos-anchor-placement="top-center"
-        >
-          {{ data?.desc }}
-        </h2>
-
-        <div
-          class="mt-4 sm:text-sm xss:text-xs text-[10px]"
-          data-aos-anchor-placement="top-center"
-        >
-          <p class="font-title text-lg sm:text-2xl md:text-3xl capitalize">
-            {{ data?.title }}
-          </p>
-
-          <p class="font-title text-base md:text-2xl capitalize">
-            Created by : {{ getDataUser }}
-          </p>
-
-          <p class="sm:mt-2 text-[12px] xs:text-base">
-            {{ time(data?.createdStory) }}
-          </p>
+  <TemplatePanel>
+    <template #image>
+      <div class="flex justify-center mb-5 md:mb-9">
+        <div class="w-44 md:w-64 mx-auto">
+          <img
+            v-if="data?.image[0]?.url"
+            v-lazy="data && data?.image[0]?.url"
+            class="object-cover bg-center grayscale"
+            :alt="data?.title"
+          >
         </div>
       </div>
-    </div>
-  </section>
+    </template>
+
+    <template #title>
+      <p class="font-title text-lg sm:text-2xl md:text-3xl capitalize text-center">
+        {{ data?.title }}
+      </p>
+    </template>
+
+    <template #desc>
+      <div class="text-center">
+        <div class="">
+          {{ data?.desc }}
+        </div>
+
+        <span class="text-gray-600 block mt-5 text-xs">
+          {{ time(data?.createdStory) }}
+        </span>
+      </div>
+    </template>
+  </TemplatePanel>
 </template>
---
+++ src/components/homepages/TemplatePanel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-screen relative mb-44">
+    <div class="absolute top-20 w-full">
+      <div class="mx-auto w-11/12 md:w-1/2">
+        <slot name="image" />
+
+        <slot name="title" />
+
+        <slot name="desc" />
+      </div>
+    </div>
+  </div>
+</template>
